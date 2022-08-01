6c3c68f16bf54f1e70e86ef512bca06211733fd5
Emma <emma@ubuntu.Home>
2022-08-01 21:26:25 +0100

    updated alert test

--- src/tests/components/Alert.test.js
+++ src/tests/components/Alert.test.js
@@ -2,22 +2,29 @@ import React from "react";
 import { render } from "@testing-library/react";
 import Alert from "../../tests/components/Alert";
 
-test("displays an error message", () => {
-  const { getByText, asFragment } = render(<Alert message="Error!" />);
+describe("Alert", () => {
+  test("displays an error message", () => {
+    const { getByText, asFragment } = render(<Alert message="Error!" />);
+    const alert = asFragment();
 
-  expect(getByText(/Error/).toBeContent).toBe("Error!");
-});
+    expect(getByText(/Error/).toBeContent).toBe("Error!");
+    expect(alert).toMatchSnapshot();
+  });
 
-test("displays a success message", () => {
-  const { getByText, asFragment } = render(
-    <Alert message="Success!!!!" success />
-  );
+  test("displays a success message", () => {
+    const { getByText, asFragment } = render(
+      <Alert message="Success!!!!" success />
+    );
+    const alert = asFragment();
 
-  expect(getByText(/Success/).textContent).toBe("Success!!!!");
-});
+    expect(getByText(/Success/).textContent).toBe("Success!!!!");
+    expect(alert).toMatchSnapshot();
+  });
 
-test("does not render an error or success message if message props is empty", () => {
-  const { asFragment } = render(<Alert message="" />);
-  const alert = asFragment();
-  expect(alert).toMatchSnapshot();
+  test("does not render an error or success message if message props is empty", () => {
+    const { asFragment } = render(<Alert message="" />);
+    const alert = asFragment();
+    
+    expect(alert).toMatchSnapshot();
+  });
 });
