21338deefe666e39338e75d944dcb41bda89241a
Emma <emma@ubuntu.Home>
2022-08-02 10:37:22 +0100

    created properties and property card

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,6 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import PropertyCard from "./PropertyCard";
+import "../styles/properties.css";
+import Alert from "./Alert";
+
+const initialState = {
+  properties: [],
+};
 
 const Properties = () => {
-  return <div className="AddProperty">Properties Page</div>;
+  const [properties, setProperties] = useState(initialState.properties);
+  const { alert, setAlert } = useState(initialState.alert);
+
+  // const endpoint = "http://localhost:3000/api/v1/PropertyListing";
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/api/v1/PropertyListings`)
+      .then(({ data }) => setProperties(data))
+      .catch(() =>
+        setAlert({
+          message: "Property not found, try again!",
+          isSuccess: false,
+        })
+      );
+  }, []);
+
+  return (
+    <div>
+      <div>Properties Page</div>
+      <div className="propertyGrid">
+        <Alert message={alert.message} success={alert.isSuccess} />
+        {properties.map((property) => (
+          <PropertyCard {...property} key={property.price} />
+        ))}
+      </div>
+      ;
+    </div>
+  );
 };
 export default Properties;
