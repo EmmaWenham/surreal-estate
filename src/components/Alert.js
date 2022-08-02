44ed6fc5073a80a00089c68a4ef8a38892d802f2
Emma <emma@ubuntu.Home>
2022-08-02 09:43:41 +0100

    created property card and tests

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 
 const Alert = ({ message, success }) => {
   if (!message) return null;
+
   return (
     <div className={`alert alert-${success ? "success" : "error"}`}>
       {message}
@@ -12,11 +13,11 @@ const Alert = ({ message, success }) => {
 
 export default Alert;
 
-Alert.PropTypes = {
-  message: PropTypes.string.isRequired,
-  success: PropTypes.bool,
-};
-
 Alert.defaultProps = {
   success: false,
+};
+
+Alert.propTypes = {
+  message: PropTypes.string.isRequired,
+  success: PropTypes.bool,
 };
